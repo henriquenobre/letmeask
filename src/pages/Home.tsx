49893cedf0c708f0febf77bd479d1c2e3cdf5998
Henrique Nobre <henriquenobr@gmail.com>
2022-01-11 16:06:18 -0300

    autentication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,23 @@ import ilustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIcon from '../assets/images/google-icon.svg'
 import { Button } from '../components/Button'
+import { useNavigate } from 'react-router-dom'
 
 import '../style/auth.css'
+import { useAuth } from '../hooks/useAuth'
 
 export function Home() {
+
+    const navigate = useNavigate()
+    const { user, signInWithGoogle } = useAuth()
+
+    async function handleCreateRoom() {
+      if (!user) {
+        await signInWithGoogle()
+      }
+      navigate('/rooms/new')
+    }
+
     return(
         <div id='page-auth'>
             <aside>
@@ -16,7 +29,7 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="leteme" />
-                    <button className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIcon} alt="logo google" />
                         Crie sua sala com o Google
                     </button>
