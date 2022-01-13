3c1176dd35985adbbfe5c1444a92a591d64c9353
Henrique Nobre <henriquenobr@gmail.com>
2022-01-13 16:09:53 -0300

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,20 +6,40 @@ import { useNavigate } from 'react-router-dom'
 
 import '../style/auth.css'
 import { useAuth } from '../hooks/useAuth'
+import { FormEvent, useState } from 'react'
+import { database } from '../services/firebase'
 
 export function Home() {
 
     const navigate = useNavigate()
     const { user, signInWithGoogle } = useAuth()
+    const [roomCode, setRoomCode] = useState('')
 
     async function handleCreateRoom() {
-      if (!user) {
-        await signInWithGoogle()
-      }
-      navigate('/rooms/new')
+        if (!user) {
+            await signInWithGoogle()
+        }
+        navigate('/rooms/new')
     }
 
-    return(
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault()
+
+        if (roomCode.trim() === '') {
+            return
+        }
+        const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+        if (!roomRef.exists()) {
+            alert('Room does not exist')
+            return
+        }
+
+        navigate(`/rooms/${roomCode}`)
+
+    }
+
+    return (
         <div id='page-auth'>
             <aside>
                 <img src={ilustrationImg} alt="ilustracao" />
@@ -34,8 +54,12 @@ export function Home() {
                         Crie sua sala com o Google
                     </button>
                     <div className="separetor">ou entre em uma sala</div>
-                    <form >
-                        <input type="text" placeholder='Digite o código da sala' />
+                    <form onSubmit={handleJoinRoom}>
+                        <input type="text"
+                            placeholder='Digite o código da sala'
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
+                        />
                         <Button type='submit'>Entrar na sala</Button>
                     </form>
                 </div>
