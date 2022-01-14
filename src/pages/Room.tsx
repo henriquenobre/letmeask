69c4b8f892a162af2694243d8f4db08948da5654
Henrique Nobre <henriquenobr@gmail.com>
2022-01-14 16:07:32 -0300

    home

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,43 @@
+import logoImg from '../assets/images/logo.svg'
+
 export function Room () {
     return(
-        <h1>SALA</h1>
+        <div id="page-room">
+        <header>
+          <div className="content">
+            <img src={logoImg} alt="Letmeask" />
+            <RoomCode code={roomId} />
+          </div>
+        </header>
+  
+        <main>
+          <div className="room-title">
+            <h1>Sala {title}</h1>
+            { questions.length > 0 && <span>{questions.length} pergunta(s)</span> }
+          </div>
+  
+          <form onSubmit={handleSendQuestion}>
+            <textarea
+              placeholder="O que você quer perguntar?"
+              onChange={event => setNewQuestion(event.target.value)}
+              value={newQuestion}
+            />
+  
+            <div className="form-footer">
+              { user ? (
+                <div className="user-info">
+                  <img src={user.avatar} alt={user.name} />
+                  <span>{user.name}</span>
+                </div>
+              ) : (
+                <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+              ) }
+              <Button type="submit" disabled={!user}>Enviar pergunta</Button>
+            </div>
+          </form>
+  
+          {JSON.stringify(questions)}
+        </main>
+      </div>
     )
 }
