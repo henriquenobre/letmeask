db4912293505c3785885ab2ee93771d9db9db290
Henrique Nobre <henriquenobr@gmail.com>
2022-01-24 11:36:38 -0300

    room

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,4 +1,5 @@
 import logoImg from '../assets/images/logo.svg'
+import { Button } from '../components/Button'
 
 export function Room () {
     return(
@@ -6,37 +7,29 @@ export function Room () {
         <header>
           <div className="content">
             <img src={logoImg} alt="Letmeask" />
-            <RoomCode code={roomId} />
+            
           </div>
         </header>
   
         <main>
           <div className="room-title">
-            <h1>Sala {title}</h1>
-            { questions.length > 0 && <span>{questions.length} pergunta(s)</span> }
+            <h1>Sala React</h1>
+            <span>4 pergunta(s)</span> 
           </div>
   
-          <form onSubmit={handleSendQuestion}>
+          <form>
             <textarea
               placeholder="O que você quer perguntar?"
-              onChange={event => setNewQuestion(event.target.value)}
-              value={newQuestion}
             />
   
             <div className="form-footer">
-              { user ? (
-                <div className="user-info">
-                  <img src={user.avatar} alt={user.name} />
-                  <span>{user.name}</span>
-                </div>
-              ) : (
+             
                 <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
-              ) }
-              <Button type="submit" disabled={!user}>Enviar pergunta</Button>
+             
+              <Button type="submit">Enviar pergunta</Button>
             </div>
           </form>
   
-          {JSON.stringify(questions)}
         </main>
       </div>
     )
